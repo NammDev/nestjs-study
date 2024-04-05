2634a2408d132a81208877b26021d04dfdb2ef80
Nam <namdeveloper.ca@gmail.com>
2024-04-05 12:25:22 +0700

    validationPipe

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,6 +6,8 @@ import {
   Param,
   Delete,
   Patch,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
@@ -26,6 +28,7 @@ export class TasksController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.tasksService.createTask(createTaskDto);
   }
