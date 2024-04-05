e3fcacbb4b3b27d3d41a7df930851bb27031cce0
Nam <namdeveloper.ca@gmail.com>
2024-04-05 10:54:03 +0700

    defining a task model

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = ['do homework', 'do laundry', 'do dishes'];
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'Task 1',
+      description: 'Description 1',
+      status: TaskStatus.OPEN,
+    },
+    {
+      id: '2',
+      title: 'Task 2',
+      description: 'Description 2',
+      status: TaskStatus.IN_PROGRESS,
+    },
+    {
+      id: '3',
+      title: 'Task 3',
+      description: 'Description 3',
+      status: TaskStatus.DONE,
+    },
+  ];
 
   getAllTasks() {
     return this.tasks;
