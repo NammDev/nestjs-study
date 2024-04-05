16653bf087f7d457cd7a7b0caea02619101329e3
Nam <namdeveloper.ca@gmail.com>
2024-04-05 12:23:03 +0700

    dtp to dto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,7 +9,7 @@ import {
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
-import { CreateTaskDto } from './dtp/create-task.dto';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
