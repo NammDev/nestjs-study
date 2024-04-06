7b35876d03d00d7bd3d997bf25463a15670645c1
Nam <namdeveloper.ca@gmail.com>
2024-04-06 17:32:34 +0700

    validation authCredentialAuth, password regular

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password too weak',
+  })
+  // Password must contain at least one upper case letter, one lower case letter, one number or special character, and must be at least 8 characters long
   password: string;
 }
