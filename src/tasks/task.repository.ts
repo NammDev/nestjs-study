624085f1133c42082293f7867f772b01e5ca895e
Nam <namdeveloper.ca@gmail.com>
2024-04-06 12:53:05 +0700

    olution to this which allows to call custom methods but also inherited ones

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,5 +1,25 @@
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task-status.enum';
+import { Task } from './task.entity';
 
 @Injectable()
-export class TaskRepository<TaskEntity> extends Repository<TaskEntity> {}
+export class TaskRepository extends Repository<Task> {
+  constructor(private dataSource: DataSource) {
+    super(Task, dataSource.createEntityManager());
+  }
+
+  /**
+   * Add a basic to create new Task
+   */
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const { title, description } = createTaskDto;
+    const task = new Task();
+    task.title = title;
+    task.description = description;
+    task.status = TaskStatus.OPEN;
+    await task.save();
+    return task;
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,18 +1,19 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { TaskRepository } from './task.repository';
 import { Task } from './task.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
-  constructor(
-    @InjectRepository(Task)
-    private taskRepository: TaskRepository<Task>,
-  ) {}
+  constructor(private taskRepository: TaskRepository) {}
 
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne({ where: { id } });
     if (!found) throw new NotFoundException(`Task with ID "${id}" not found`);
     return found;
+  }
+
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.taskRepository.createTask(createTaskDto);
   }
 }
