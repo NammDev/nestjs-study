4f71799a284e5a1d3f9bb96eaed097591642e47b
Nam <namdeveloper.ca@gmail.com>
2024-04-07 19:13:15 +0700

    return accessToken for FE, then pass it on with every request

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,30 @@
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto) {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const username =
       await this.userRepository.validateUserPassword(authCredentialsDto);
     if (!username) throw new UnauthorizedException('Invalid credentials');
+
+    const payload: JwtPayload = { username };
+    const accessToken = this.jwtService.sign(payload);
+
+    return { accessToken };
   }
 }
