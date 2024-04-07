f8a8182089c099dfdb2e0f997354920ce2d49457
Nam <namdeveloper.ca@gmail.com>
2024-04-07 21:31:47 +0700

    Setting up the JWT Strategy for Authorization

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { JwtPayload } from './jwt-payload.interface';
+import { UserRepository } from './user.repository';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { User } from './user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private userRepository: UserRepository) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'topSecret51',
+      ignoreExpiration: false,
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { username } = payload;
+    const user = await this.userRepository.findOne({ where: { username } });
+    if (!user) throw new UnauthorizedException();
+    return user;
+  }
+}
